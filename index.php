7a0a49f0e965694073f8b418a85c908accf7b151
VitoriaMinotti <vitorialminotti@hotmail.com>
2023-09-02 19:27:59 -0300

    ajuste na tela de venda

--- index.php
+++ index.php
@@ -24,11 +24,11 @@
     <!-- tela de pesquisa, só aparece ao clicar no botão de pesquisar -->
     <div class="container" style="display: none;" id="resultadoPesquisa">
     <br>
-    <div class="form-group">
-        <label for="saldo">Saldo:</label>
-        <input type="text" class="form-control" id="saldo" readonly>
+    <div id="saldo">
+        <h4>Saldo</h4>
+        <span id="saldoValor"></span>
     </div>
-    <table class="table mt-3" id="tabelaOperacoes" >
+        <table class="table mt-3" id="tabelaOperacoes" >
         <thead>
             <tr>
                 <th>Operações</th>
@@ -60,6 +60,26 @@
                 <button type="submit" class="btn btn-success">Confirmar</button>
             </form>
                 <button type="button" class="btn btn-danger" onclick="cancelarCompra()">Cancelar</button>
+            </div>
+    </div>
+
+    <!-- tela de venda de ativo, só aparece ao clicar no botão de venda -->
+    <div class="container form mt-3" id="telaVenda" style="display: none;">
+        <h2 style="color:red;">Venda de Ativo</h2>
+        <form action="salvar_venda.php" method="post">
+            <input type="text" name="ativoVenda" id="ativoVenda" placeholder="Ativo" readonly>
+            <label for="valorAtivoVenda">Valor do Ativo:</label>
+            <span id="valorAtivoVenda"></span>
+            <input type="number" name="quantidade" id="quantidadeVenda" placeholder="Quantidade" oninput="calcularValorOrdem('venda')">
+            <label for="valorOrdem">Valor da Ordem:</label>
+            <span id="valorOrdem" name="valorOrdemVenda"></span>
+            <input type="hidden" id="valorOrdemHidden" name="valorOrdemVenda"/>
+            <input type="hidden" name="tipo" value="2"> <!-- 2 para Venda -->
+            <div class="container">
+                <br>
+                <button type="submit" class="btn btn-success">Confirmar</button>
+            </form>
+                <button type="button" class="btn btn-danger" onclick="cancelarVenda()">Cancelar</button>
             </div>
     </div>
 </div>
--- script.js
+++ script.js
@@ -44,22 +44,73 @@ if (ativo.trim() !== "") { //usa o trim para verificar os espaços em branco
 }
 });
 
-// Função para calcular o valor da ordem de compra quando o usuário digitar a quantidade de ativos
-function calcularValorOrdem(tipo) {
-    var valorAtivoElement = tipo === 'venda' ? document.getElementById("valorAtivoVenda") : document.getElementById("valorAtivoCompra"); // Verificar se é compra ou venda 
-    var quantidadeElement = tipo === 'venda' ? document.getElementById("quantidadeVenda") : document.getElementById("quantidadeCompra"); 
-    var valorOrdemSpan = tipo === 'venda' ? document.getElementById("valorOrdemVenda") : document.getElementById("valorOrdem"); 
-    
-    var valorAtivo = parseFloat(valorAtivoElement.textContent); 
-    var quantidade = parseFloat(quantidadeElement.value);
-    
-    if (!isNaN(valorAtivo) && !isNaN(quantidade)) { 
-        var valorOrdem = quantidade * valorAtivo;
-        valorOrdemSpan.textContent = valorOrdem.toFixed(2);
-        document.getElementById('valorOrdemHidden').value = valorOrdem.toFixed(2); // Atualizar o campo oculto com o valor da ordem de compra ou venda 
+// Mostrar a tela de venda de ativos quando clicar no botão "Vender"
+document.getElementById("vender").addEventListener("click", function() {
+    var ativo = document.getElementById("ativo").value; // Salva o que foi inserido no input de ativo
+
+    if (ativo.trim() !== "") { // Verifica se o campo de ativo não está em branco
+        fetch("verificar_ativo.php", { // Faz a requisição para o PHP para verificar se o ativo existe
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: "ativo=" + encodeURIComponent(ativo)
+        })
+        .then(response => response.json())
+        .then(data => { // Verifica se o ativo existe no banco de dados
+            if (data.exists) {
+                // Se o ativo existe no banco de dados, exibir a tela de venda
+                document.getElementById("resultadoPesquisa").style.display = "none"; // Esconde a tela de pesquisa
+                document.getElementById("telaCompra").style.display = "none"; // Esconde a tela de compra
+                document.getElementById("telaVenda").style.display = "block"; // Mostra a tela de venda
+                document.getElementById("ativoVenda").value = ativo; // Preenche o campo de ativo na tela de venda
+
+                // Consultar o banco de dados para obter o valor do ativo
+                fetch("verificar_ativo.php", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    body: "ativo=" + encodeURIComponent(ativo)
+                })
+                .then(response => response.json())
+                .then(data => { // Preenche o valor do ativo na tela de venda
+                    if (data.preco) {
+                        document.getElementById("valorAtivoVenda").textContent = data.preco;
+                    }
+                })
+                .catch(error => console.error("Erro na requisição: " + error));
+            } else {
+                alert("Ativo não encontrado no banco de dados.");
+            }
+        })
+        .catch(error => console.error("Erro na requisição: " + error));
     } else {
-        valorOrdemSpan.textContent = "Insira valores numéricos válidos.";
-        document.getElementById('valorOrdemHidden').value = ""; // Resetar o campo oculto se houver um erro
+        alert("Preencha o campo 'Ativo' antes de vender.");
+    }
+});
+
+// Função para calcular o valor da ordem de compra ou venda quando o usuário digitar a quantidade de ativos
+function calcularValorOrdem(tipo) {
+    var valorAtivoElement = tipo === 'venda' ? document.getElementById("valorAtivoVenda") : document.getElementById("valorAtivoCompra");
+    var quantidadeElement = tipo === 'venda' ? document.getElementById("quantidadeVenda") : document.getElementById("quantidadeCompra");
+    var valorOrdemSpan = tipo === 'venda' ? document.getElementById("valorOrdemVenda") : document.getElementById("valorOrdem");
+
+    // Verifica se os elementos existem antes de acessá-los
+    if (valorAtivoElement && quantidadeElement && valorOrdemSpan) {
+        var valorAtivo = parseFloat(valorAtivoElement.textContent);
+        var quantidade = parseFloat(quantidadeElement.value);
+
+        if (!isNaN(valorAtivo) && !isNaN(quantidade)) {
+            var valorOrdem = quantidade * valorAtivo;
+            valorOrdemSpan.textContent = valorOrdem.toFixed(2);
+            document.getElementById('valorOrdemHidden').value = valorOrdem.toFixed(2);
+        } else {
+            valorOrdemSpan.textContent = "Insira valores numéricos válidos.";
+            document.getElementById('valorOrdemHidden').value = "";
+        }
+    } else {
+        console.error("Elementos não encontrados.");
     }
 }
 
@@ -74,14 +125,26 @@ function cancelarCompra() {
     document.getElementById('telaCompra').style.display = 'none';
 }
 
-// Função para cancelar venda quando clicar no botão cancelar
-function cancelarVenda() {
-    document.getElementById("telaVenda").style.display = "none"; // Esconde a tela de compra
-     // Apaga o número digitado no campo de quantidade
-     document.getElementById("quantidade").value = "";
-  }
-
 //função para pesquisar o ativo	
+function calcularSaldo(operacoes) {
+    let saldo = 0;
+
+    operacoes.forEach(function (operacao) {
+        const tipo = operacao.tipo === 1 ? "Compra" : "Venda";
+        const quantidade = parseInt(operacao.quantidade);
+        const valor = parseFloat(operacao.valor);
+
+        if (tipo === "Compra") {
+            saldo += quantidade * valor;
+        } else {
+            saldo -= quantidade * valor;
+        }
+    });
+
+    return saldo;
+}
+
+// Função para pesquisar o ativo
 document.querySelector("[name='pesquisar']").addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -162,6 +225,7 @@ document.querySelector("[name='pesquisar']").addEventListener("click", function(
     .catch(error => console.error("Erro na requisição: " + error));
 });
 
+
 // Função para limpar a pesquisa 
 function limparPesquisa() {
     document.getElementById("ativo").value = ""; // Limpa o input
@@ -181,23 +245,3 @@ function limparPesquisa() {
     document.getElementById("telaVenda").style.display = "none"; // Esconde a tela de venda
     document.getElementById("telaCompra").style.display = "none"; // Esconde a tela de compra
 }
-
-// Função para calcular o saldo com base nas operações
-function calcularSaldo(operacoes) {
-    var saldo = 0;
-
-    operacoes.forEach(function(operacao) {
-        var tipo = operacao.tipo === 1 ? "Compra" : "Venda";
-        var quantidade = parseInt(operacao.quantidade);
-        var valorTotal = parseFloat(operacao.valor);
-
-        if (tipo === "Compra") {
-            saldo += quantidade * valorTotal;
-        } else if (tipo === "Venda") {
-            saldo -= quantidade * valorTotal;
-        }
-    });
-
-    return saldo;
-}
-
